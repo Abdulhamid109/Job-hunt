a1694333712c5661f06ee1fa844064b7df79ef63
Abdulhamid Patel <2000147400@hexaware.com>
2025-08-04 10:30:33 +0530

    reflecting changes

--- src/models/userModal.ts
+++ src/models/userModal.ts
@@ -2,5 +2,17 @@ import mongoose from "mongoose";
 
 
 const userMd = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter the name"]
+    },
+    email:{
+        type:String,
+        required:[true,"Please enter the email"]
+    },
+    password:{
+        type:String,
+        required:[true,"Please Enter your Password"]
+    },
     
 });
